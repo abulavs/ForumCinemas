7100232e5ac9148f54505d0af904919795ed085c
abulavs <andris.bulavs@gmail.com>
2022-05-06 13:31:54 +0300

    update

--- src/test/java/pages/ProfilePageForum.java
+++ src/test/java/pages/ProfilePageForum.java
@@ -7,41 +7,41 @@ import org.openqa.selenium.support.ui.Select;
 
 public class ProfilePageForum extends Common {
 
-    private final By UserName = By.xpath("//input[@name='userName']");
-    private final By Password = By.xpath("//input[@name='password']");
-    private final By LoginButton = By.xpath("//div[4]/button");
-    private final By GoToProfileButton = By.xpath("//li/a/span");
-    private final By ProfileEditButton = By.xpath("//a[contains(@href, '/mypage/profile/')]");
-    private final By UserFirstName = By.id("inputFirstName");
-    private final By UserLastName = By.id("inputLastName");
-    private final By SubmitProfileButton = By.xpath("(//button[@type='submit'])[3]");
+    private final By userName = By.xpath("//input[@name='userName']");
+    private final By password = By.xpath("//input[@name='password']");
+    private final By loginButton = By.xpath("//div[4]/button");
+    private final By goToProfileButton = By.xpath("//li/a/span");
+    private final By profileEditButton = By.xpath("//a[contains(@href, '/mypage/profile/')]");
+    private final By userFirstName = By.id("inputFirstName");
+    private final By userLastName = By.id("inputLastName");
+    private final By submitProfileButton = By.xpath("(//button[@type='submit'])[3]");
 
 
     public void enterUsernamePassword(String username, String password) {
-        driver.findElement(UserName).sendKeys(username);
-        driver.findElement(Password).sendKeys(password);
+        driver.findElement(userName).sendKeys(username);
+        driver.findElement(this.password).sendKeys(password);
     }
 
     public void submitLogin() {
-        driver.findElement(LoginButton).click();
+        driver.findElement(loginButton).click();
     }
 
     public void goToEditProfile() {
-        driver.findElement(GoToProfileButton).click();
-        driver.findElement(ProfileEditButton).click();
+        driver.findElement(goToProfileButton).click();
+        driver.findElement(profileEditButton).click();
     }
 
     public void changeUserName(String firstName, String lastName) {
-        driver.findElement(UserFirstName).clear();
-        driver.findElement(UserFirstName).sendKeys(firstName);
-        driver.findElement(UserLastName).clear();
-        driver.findElement(UserLastName).sendKeys(lastName);
+        driver.findElement(userFirstName).clear();
+        driver.findElement(userFirstName).sendKeys(firstName);
+        driver.findElement(userLastName).clear();
+        driver.findElement(userLastName).sendKeys(lastName);
     }
 
     public void compareUserName(String firstName, String lastName) {
 
-        String savedName = driver.findElement(UserFirstName).getAttribute("value");
-        String savedSurname = driver.findElement(UserLastName).getAttribute("value");
+        String savedName = driver.findElement(userFirstName).getAttribute("value");
+        String savedSurname = driver.findElement(userLastName).getAttribute("value");
 
         if (savedName.equals(firstName) && savedSurname.equals(lastName)) {
             System.out.println("Name and Surname are correct");
@@ -63,6 +63,6 @@ public class ProfilePageForum extends Common {
     public void submitChanges() {
         JavascriptExecutor js = driver;
         js.executeScript("window.scrollBy(0,1500)");
-        driver.findElement(SubmitProfileButton).click();
+        driver.findElement(submitProfileButton).click();
     }
 }
